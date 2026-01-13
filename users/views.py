442b2557581eb4fc3bd587df2aad63fa37b489d1
abduqodir <ttheonepersonn@gmail.com>
2026-01-13 18:57:43 +0500

    views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics , permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializer import Userregister , TokenObtainPairSerializer ,CustomTokenObtainPairSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class Register(generics.CreateAPIView):
+    queryset = User.objects.all
+    serializer_class = Userregister
+    
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class  = CustomTokenObtainPairSerializer
