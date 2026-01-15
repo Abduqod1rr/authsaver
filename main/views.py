4646472bd4012552749ef9800a8bc025c6343fde
abduqodir <ttheonepersonn@gmail.com>
2026-01-15 15:16:25 +0500

    basics

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import accdata 
+from .serializer import AccDataSeriliazers
+from rest_framework.generics import CreateAPIView ,DestroyAPIView
+from .permissions import IsOwner
 
-# Create your views here.
+
+class createData(CreateAPIView):
+    queryset = accdata.objects.all()
+    serializer_class = AccDataSeriliazers
+    permission_classes = [IsOwner]
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
